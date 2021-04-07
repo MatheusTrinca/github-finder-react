785d63c47f2eb8212cc6a8020de7b95405b82a7b
Nalu e Matheus <NaluMatheus@MacBook-de-Nalu.local>
2021-04-07 16:33:18 -0300

    prepare for deploy

--- src/api/github.js
+++ src/api/github.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
 
+let githubClientID;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientID = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 export default axios.create({
   baseURL: 'https://api.github.com',
   params: {
-    client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
-    secret_id: process.env.REACT_APP_GITHUB_CLIENT_SECRET,
+    client_id: githubClientID,
+    secret_id: githubClientSecret,
   },
 });
