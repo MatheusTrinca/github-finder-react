431954773d109ed7f9d5c81789b584862f38e31a
Nalu e Matheus <NaluMatheus@MacBook-de-Nalu.local>
2021-04-07 11:20:22 -0300

    single user e repos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import github from './api/github';
 import SearchUsers from './components/users/SearchUsers';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import User from './components/users/User';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -17,6 +22,20 @@ class App extends Component {
     this.setState({ loading: true });
     const response = await github.get(`/search/users?q=${text}`);
     this.setState({ users: response.data.items, loading: false });
+  };
+
+  getUser = async (username) => {
+    this.setState({ loading: true });
+    const response = await github.get(`/users/${username}`);
+    this.setState({ user: response.data, loading: false });
+  };
+
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const response = await github.get(
+      `/users/${username}/repos?per_page=5&sort=created:asc`
+    );
+    this.setState({ repos: response.data, loading: false });
   };
 
   clearUsers = () => {
@@ -29,21 +48,48 @@ class App extends Component {
   };
 
   render() {
-    const { alert, users, loading } = this.state;
+    const { alert, users, user, loading, repos } = this.state;
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <SearchUsers
-            showClear={users.length > 0 ? true : false}
-            clearUsers={this.clearUsers}
-            onSearch={this.onSearch}
-            showAlert={this.showAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <SearchUsers
+                      showClear={users.length > 0 ? true : false}
+                      clearUsers={this.clearUsers}
+                      onSearch={this.onSearch}
+                      showAlert={this.showAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/users/:login"
+                render={(props) => (
+                  <User
+                    {...props}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>Github Finder App</h2>
+      <p>This is an app that search Github users</p>
+      <p>version 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;
